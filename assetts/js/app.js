b7654652d7bad38bf3c12ec6ca7df34d603452d5
davepump <46700142+dcabrera97@users.noreply.github.com>
2019-04-10 21:55:10 -0700

    update and add files

---
+++ assetts/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    function expand() {
+        $(".search").toggleClass("close");
+        $(".input").toggleClass("square");
+        if ($('.search').hasClass('close')) {
+            $('input').focus();
+        } else {
+            $('input').blur();
+        }
+    }
+    $('button').on('click', expand);
+})
