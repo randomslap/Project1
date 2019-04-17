6c1ceef9b37d5dad14f43ff9f773fb50206a54f1
randomslap <kentokazaki@gmail.com>
2019-04-17 02:12:58 -0700

    update

--- assets/js/dota.js
+++ assets/js/dota.js
@@ -20,9 +20,17 @@ function ajax() {
 	var queryURL =
 		"https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=7D56B7CA02EB88A0A266FB4E5F61385B&vanityurl=" +
 		player;
+
 	$.ajax({
 		url: queryURL,
-		method: "GET"
+		method: "GET",
+		withCredentials: true,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET"
+		},
+		dataType: "json",
+		crossDomain: true
 	}).then(function(response) {
 		console.log(response);
 	});
